5f0c4306a8e4831c760b021e85ab09046a7a2f03
edgardlv <89505353+edgardlv@users.noreply.github.com>
2021-08-25 01:52:43 -0500

    Agrego cambio
    Agrego un nuevo cambio

--- js/app.js
+++ js/app.js
@@ -366,6 +366,14 @@ window.onload = function () {
   }
 
 
+  if ('serviceWorker' in navigator) {   
+    window.addEventListener('load', () => {     
+        navigator.serviceWorker.register('../sw.js').then( () => {       
+            console.log('Service Worker Registered')     
+            });   
+        }); 
+    }
+
   
   
   
